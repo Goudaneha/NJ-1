296dfcc81d488b28dc2154e5fc365dc55412f91c
goudaneha <janagoudarani@gmail.com>
2020-09-03 03:48:40 -0700

    driver add

--- src/test/java/com/zoopla/test/Propertytest.java
+++ src/test/java/com/zoopla/test/Propertytest.java
@@ -42,12 +42,12 @@ public class Propertytest extends TestBase{
 	@Description("Test case description : Zoopla Title Test")
 	@Story("Story Name : Varify Title Test")
 	public void Varifytitletest() throws InterruptedException {
-	//	log.info("****************starts Property page title test*********************");
+		log.info("****************starts Property page title test*********************");
 		String proptitle = prop_page.ZooplapropTitle();	
 		Assert.assertEquals(proptitle, "Property for Sale in London - Buy Properties in London - Zoopla");
 		Thread.sleep(2000);
 		System.out.println("HeaderTitleTest : " + proptitle);
-//		log.info("****************ending Property page title test*********************");
+	log.info("****************ending Property page title test*********************");
 		//Thread.sleep(3000);
 	}
 	
@@ -56,20 +56,20 @@ public class Propertytest extends TestBase{
 	@Description("Test case description : Zoopla Property List Test")
 	@Story("Story Name : Varify Property List Test")
 	public void VarifyListofPropTest() throws InterruptedException {
-//		log.info("****************strats Property list values test*********************");
+	log.info("****************strats Property list values test*********************");
 		boolean logotest = prop_page.Zooplalogo();
 	    Assert.assertTrue(logotest);
 	    System.out.println(logotest);
-//		log.info("****************ending Property list values test*********************");
+		log.info("****************ending Property list values test*********************");
 
 	}
 	
 	@Test(priority =3,description = "Get List of Property values")
 	public void VarifyPropertyListValuesTest() {
-//		log.info("****************strats print Property values test*********************");
+		log.info("****************strats print Property values test*********************");
 		//System.out.println("Total no of property values"+ prop_page.listofprop);
 		prop_page.Zooplalistofpropvalues();
-//		log.info("****************ending print Property values test*********************");
+		log.info("****************ending print Property values test*********************");
 
 	}
 	@Test(priority = 4,description = "Get Fifty property values from list")
@@ -77,15 +77,15 @@ public class Propertytest extends TestBase{
 	@Description("Test case description : Zoopla Fifty property value Test")
 	@Story("Story Name : Varify Fifty property value Test")
 	public void Varify5thPropertyValuesTest() throws InterruptedException {
-//		log.info("****************starts click on fifth Property values test*********************");
+		log.info("****************starts click on fifth Property values test*********************");
 		prop_page.ZooplaFifthPropValue();
-	//	log.info("****************ending click on fifth Property values test*********************");
+    	log.info("****************ending click on fifth Property values test*********************");
 
 	}
 	
 	@AfterTest
 	private void teardown() {
-//	log.info("***************************Test Execution Terminates******************************");
-	driver.close();
+	log.info("***************************Test Execution Terminates******************************");
+	driver.quit();
 	}
 }
